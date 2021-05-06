ace247b998858a4e753e1de27101a1493023ac7d
schang729 <80146189+schang729@users.noreply.github.com>
2021-05-06 10:44:58 -0700

    hw

---
+++ 01-java-fundamentals/02-pythagorean-theorem/Pythagorean.java
@@ -0,0 +1,9 @@
+public class Pythagorean {
+    public double calculateHypotenuse(int legA, int legB) {
+
+        double legC = (legA * legA) + (legB * legB);
+        return Math.sqrt(legC);
+        
+
+    }
+}
---
+++ 01-java-fundamentals/02-pythagorean-theorem/PythagoreanTest.java
@@ -0,0 +1,9 @@
+public class PythagoreanTest {
+    public static void main(String[] args) {
+        Pythagorean result = new Pythagorean();
+        double lenC = result.calculateHypotenuse(10, 20);
+        System.out.println(lenC);
+
+    }
+
+}
