70c22fc994fedd6bab64d5c1426b6399f0399ba7
schang729 <80146189+schang729@users.noreply.github.com>
2021-05-07 10:51:03 -0700

    hw

---
+++ 01-java-fundamentals/03-fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,9 @@
+public class FizzBuzzTest {
+    public static void main(String[] args) {
+        FizzBuzz result = new FizzBuzz();
+        String fb = result.fizzBuzz(5);
+        System.out.println(fb);
+
+    }
+    
+}
