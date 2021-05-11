e060ad4072177e856b6593d860a518540830a448
schang729 <80146189+schang729@users.noreply.github.com>
2021-05-11 10:56:32 -0700

    hw

---
+++ 01-java-fundamentals/08-lists-of-exceptions/ListOfExceptions.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class ListOfExceptions {
+    public static void main(String[] args) {
+        ArrayList<Object> myList = new ArrayList<Object>();
+        myList.add("13");
+        myList.add("hello world");
+        myList.add(48);
+        myList.add("Goodbye World");
+        int index = 0;
+        for (int i = 0; i < myList.size(); i++) {
+            try {
+                Integer castedValue = (Integer) myList.get(i);
+                System.out.println(castedValue);
+
+            } catch (Exception e) {
+
+                System.out.println("Index of Error: " + (index++) + " Error msg: " + e);
+
+            }
+
+        }
+
+    }
+}
